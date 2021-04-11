61e1b7e8a58955a5420755160609f751acfbf5f0
Michael <antenehnigussie1@yahoo.com>
2021-04-11 23:13:13 +0100

    Modified component. Moved a function (filterData) to another file (functions.js). Turned a function (displayStatus) into a proper component. Modified/edited function and variable names.

--- src/Components/Bookings.js
+++ src/Components/Bookings.js
@@ -2,82 +2,64 @@ import React, { useEffect, useState } from "react";
 import Search from "./Search.js";
 import NewBooking from "./NewBooking";
 import SearchResults from "./SearchResults.js";
+import BookingDataFetchStatus from "./StatusDisplay";
+import { filterData } from "../functions";
 
-const timeOut = 10; // data fetch waiting time = 5 seconds
+// CONSTANTS AND VARIABLES DECLARATION
+const timeout = 10; // data fetch waiting time = 5 seconds
 
-// Bookings component definition
+/**** THE COMPONENT ****/
 const Bookings = () => {
+  // STATE VARIABLES
   const [bookings, setBookings] = useState([]);
-  const [dataFetchStatus, setDataFetchStatus] = useState(false);
+  const [dataIsFetched, setDataIsFetched] = useState(false);
   const [delaySecondsCount, setDelaySecondsCount] = useState(0);
 
+  // EVENT HANDLERS
   const handleNewBooking = bookingData => {
-    console.log(bookingData);
-    if (bookingData) {
-      setBookings([...bookings, bookingData]);
-    }
+    bookingData["id"] = bookings.length + 1; // add order number to bookingData
+    setBookings([...bookings, bookingData]);
   };
 
-  // fetch booking data from a remote "location"
-  useEffect(() => {
-    fetch("https://cyf-react.glitch.me/delayed")
-      .then(response => response.json())
-      .then(data => {
-        setDataFetchStatus(true);
-        setBookings(data);
-      })
-      .catch(() => setDataFetchStatus(false));
-  }, []);
-
-  // check the data fetch status and decide whether to display a "wait" or error message
-  if (dataFetchStatus === false) {
-    // if data has not been fetched/loaded, wait until the end of the set timeout
-    setTimeout(() => {
-      setDelaySecondsCount(delaySecondsCount + 1);
-    }, 1000);
-    // show an error or a wait message depending on whether the timeout is over or not
-    return displayStatus(delaySecondsCount);
-  }
-
-  // on data fetch success
-  const search = searchVal => {
+  const searchBookings = searchVal => {
     console.info("TO DO!", searchVal);
     const filteredResult = filterData(bookings, searchVal);
     setBookings(filteredResult);
   };
 
-  return (
+  // USE_EFFECT
+  // fetch booking data from a remote "location"
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me/delayed")
+      .then(response => response.json())
+      .then(data => {
+        setDataIsFetched(true);
+        setBookings(data);
+      })
+      .catch(() => setDataIsFetched(false));
+  }, []);
+
+  // check the data fetch status and decide whether to display a "wait" or error message
+  if (dataIsFetched === false) {
+    // if data has not been fetched/loaded, wait until the end of the set timeout
+    setTimeout(() => {
+      setDelaySecondsCount(delaySecondsCount + 1);
+    }, 1000);
+  }
+
+  return dataIsFetched ? (
+    // on data fetch success, display booking content
     <div className="App-content">
       <div className="container">
-        <Search search={search} />
-        <NewBooking newBooking={handleNewBooking} id={bookings.length} />
+        <Search search={searchBookings} />
+        <NewBooking newBooking={handleNewBooking} />
         <SearchResults results={bookings} />
       </div>
     </div>
+  ) : (
+    // otherwise show an error or a wait message depending on whether the timeout is over or not
+    <BookingDataFetchStatus count={delaySecondsCount} timeout={timeout} />
   );
 };
 
 export default Bookings;
-
-// supplementary function definitions
-
-const displayStatus = count => {
-  if (count <= timeOut) {
-    return <p className="loading">Loading, please wait...</p>;
-  }
-  return (
-    <p className="error">
-      Sorry, cannot display booking infromation. Error while fetching data.
-    </p>
-  );
-};
-
-const filterData = (bookingDetails, userInput) => {
-  return bookingDetails.filter(customer => {
-    // make sure the search is not case-sensitive
-    const firstName = customer.firstName.toUpperCase();
-    const surname = customer.surname.toUpperCase();
-    const value = userInput.toUpperCase();
-    return firstName.includes(value) || surname.includes(value);
-  });
-};
