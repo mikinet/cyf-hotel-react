9cac4b8a336b85f1e654ff8099a9935732a5c8a0
Michael <antenehnigussie1@yahoo.com>
2021-05-07 19:21:35 +0100

    Created 2 more functions. Updated 2 functions.

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,4 @@
+import { validate } from "email-validator";
 // SUPPLEMENTARY FUNCTION DEFINITIONS
 
 export const filterData = (bookingDetails, userInput) => {
@@ -12,12 +13,18 @@ export const filterData = (bookingDetails, userInput) => {
 
 export function validateForm(form) {
   const fileds = [...form.querySelectorAll("input")];
-  return fileds
+  const errors = fileds
     .filter(input => input.value === "")
     .map(input => `${input.id} cannot be empty.`);
+  const email = document.getElementById("Email").value;
+  const emailIsValid = validate(email);
+  if (email !== "" && !emailIsValid) {
+    errors.push("Please enter a valid email address.");
+  }
+  return errors;
 }
 
-export function getFormData(form, bookingId) {
+export function getFormData(form) {
   // collect text input fields values and put them in a list
   const dataList = [...form.querySelectorAll("input")].map(
     input => input.value
@@ -26,14 +33,32 @@ export function getFormData(form, bookingId) {
   dataList.unshift(form.querySelector("select").value);
   // turn the collected list into a usable form (i.e., object)
   const formData = {
-    id: bookingId,
     title: dataList[0],
     firstName: dataList[1],
     surname: dataList[2],
     email: dataList[3],
-    roomId: dataList[4],
+    roomId: parseInt(dataList[4]),
     checkInDate: dataList[5],
     checkOutDate: dataList[6]
   };
   return formData;
+}
+
+export function createNewBooking(bookingData, url) {
+  return fetch(url, {
+    method: "POST",
+    body: JSON.stringify(bookingData),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
+
+export function getTheNextDayDate(currentDateValue) {
+  const currentDate = new Date(currentDateValue);
+  const nextDay = currentDate.setDate(currentDate.getDate() + 1);
+  const [day, month, year] = new Date(nextDay)
+    .toLocaleDateString("en-GB")
+    .split("/");
+  return [year, month, day].join("-");
 }
